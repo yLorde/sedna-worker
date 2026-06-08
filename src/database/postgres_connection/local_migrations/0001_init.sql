bf08d7cc25f3fca1b537391dba20c7ebe274359a
Davi de Sousa <davisouza2204@gmail.com>
2026-06-08 14:58:58 -0300

    add status_code

--- src/database/postgres_connection/local_migrations/0001_init.sql
+++ src/database/postgres_connection/local_migrations/0001_init.sql
@@ -1,8 +1,9 @@
 CREATE TABLE ping (
     id SERIAL NOT NULL PRIMARY KEY,
     endpoint VARCHAR(32) NOT NULL,
-    delay INT NOT NULL,
-    timeout INT NOT NULL,
-    success BOOLEAN NOT NULL,
+    delay INT DEFAULT -1,
+    timeout INT DEFAULT -1,
+    success BOOLEAN DEFAULT false,
+    status_code INT DEFAULT -1,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT timezone('brt'::text, now())
 );
--- src/libs/ping_api.rs
+++ src/libs/ping_api.rs
@@ -13,6 +13,7 @@ struct PingModel {
     delay: i32,
     timeout: i32,
     success: bool,
+    status_code: i32,
     created_at: Option<DateTime<Utc>>,
 }
 
@@ -33,12 +34,13 @@ pub async fn ping_api(db: AppState, time: u64) -> bool {
     println!("");
 
     sqlx::query_as::<_, PingModel>(
-        "INSERT INTO ping (endpoint, delay, timeout, success) VALUES ($1, $2, $3, $4) RETURNING *",
+        "INSERT INTO ping (endpoint, delay, timeout, success, status_code) VALUES ($1, $2, $3, $4, $5) RETURNING *",
     )
     .bind("/api/system/ping")
     .bind(&request_duration)
     .bind(time as i32)
     .bind(result.status().is_success())
+    .bind(result.status().as_u16() as i32)
     .fetch_one(&db.client_db)
     .await
     .unwrap();
