67eaef48e26a842551ffe69f79199cd6d3aecbb9
Davi de Sousa <davisouza2204@gmail.com>
2026-06-13 19:48:01 -0300

    Melhoria em calc_status

--- src/database/postgres_connection/mod.rs
+++ src/database/postgres_connection/mod.rs
@@ -4,6 +4,7 @@ use sqlx::{Pool, Postgres};
 
 pub async fn local_connect() -> Pool<Postgres> {
     let database_url = std::env::var("LOCAL_DATABASE_URL").expect("LOCAL_DATABASE_URL must be set");
+    let migrate_on_run = std::env::var("MIGRATE_ON_RUN").unwrap_or("false".to_string());
 
     let pool = sqlx::postgres::PgPoolOptions::new()
         .max_connections(20)
@@ -13,15 +14,17 @@ pub async fn local_connect() -> Pool<Postgres> {
         .await
         .unwrap();
 
-    let check_migrate = sqlx::migrate!("./src/database/postgres_connection/local_migrations")
-        .run(&pool)
-        .await;
+    if migrate_on_run == "true" {
+        let check_migrate = sqlx::migrate!("./src/database/postgres_connection/local_migrations")
+            .run(&pool)
+            .await;
 
-    match check_migrate {
-        Ok(_) => println!("Migrated successfully\n"),
-        Err(e) => {
-            println!("Error applying migrations: {:?} \n", e);
-            std::process::exit(1);
+        match check_migrate {
+            Ok(_) => println!("Migrated successfully\n"),
+            Err(e) => {
+                println!("Error applying migrations: {:?} \n", e);
+                std::process::exit(1);
+            }
         }
     }
 
--- src/services/calc_latency.rs
+++ src/services/calc_latency.rs
@@ -6,9 +6,17 @@ use crate::{
 pub async fn calc_latency(db: AppState) {
     println!("CALC LATENCY:");
 
+    let size_of_heartbeat_to_calc_latency: i32 =
+        std::env::var("SIZE_OF_HEARTBEATS_TO_CALC_LATENCY")
+            .unwrap_or("25".to_string())
+            .to_string()
+            .parse()
+            .unwrap();
+
     let ping_result = match sqlx::query_as::<_, HeartbeatModel>(
-        "SELECT * FROM heartbeat ORDER BY id DESC LIMIT 10",
+        "SELECT * FROM heartbeat ORDER BY id DESC LIMIT $1",
     )
+    .bind(&size_of_heartbeat_to_calc_latency)
     .fetch_all(&db.client_db)
     .await
     {
